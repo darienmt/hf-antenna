33412b7a4e396c848a6d45535f7bfcc224bf6e81
Darien Martinez Torres <darien.martinez@gmail.com>
2025-04-18 23:39:12 -0400

    Adding SE HF-360 XP with choke and without choke

--- plot_bands.py
+++ plot_bands.py
@@ -27,4 +27,40 @@ def plot_bands(path, title=""):
     fig.tight_layout()
 
 
+def plot_bands_comparative(paths, title=""):
+    files_per_band_per_day = []
+    for path in paths:
+        files_per_band = {}
+        files_per_band_per_day.append(files_per_band)
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith('.s1p'):
+                    band = file[:2]
+                    files_per_band[band] = os.path.join(root, file)
 
+    
+    fig = plt.figure(figsize=(12,20))
+    fig.suptitle(title, fontsize=16)
+    for idx, band in enumerate(bands):
+        ax = fig.add_subplot(8,2, idx + 1)
+        name = band["name"]
+        ax.set_title(f'{name} M')
+        for files_per_band in files_per_band_per_day:
+            path = files_per_band[name]
+            nw = rf.Network(path, f_unit='mhz')
+            nw.plot_s_vswr(ax = ax, label=f'{path}')     
+            print(f'{name} M : {nw}')
+        
+    
+    fig.tight_layout()
+
+def plot_all_in_one(paths, title=""):
+    fig = plt.figure(figsize=(15,5))      
+    ax = fig.add_subplot(1,1,1)
+    ax.set_title(title)
+    for path in paths:
+        nw = rf.Network(os.path.join(path, "All.s1p"), f_unit='mhz')
+        nw.plot_s_vswr(ax = ax, label=f'{path}')
+
+    # ax.get_legend().remove()
+    fig.tight_layout()
